4469ce06df95661bde601486163e58f2ebe4232d
brolin <brolin108@gmail.com>
2015-08-09 21:52:37 -0500

    Added the option to get .xlsx binary files to write to a file

--- R/drive.file.r
+++ R/drive.file.r
@@ -1,5 +1,5 @@
 #' Google Drive API /files "get" method
-#' 
+#'
 #' This function searches for and retrieves files from the Google Drive API
 #' @param download.type Which-ever format is appropriate for your document type
 #' @param title The exact title of the document you wish to download
@@ -7,23 +7,23 @@
 
 drive.file <- function(title,download.type="txt") {
 
-  file.listing <- drive.list(title,show.titles=FALSE)
-  
-  if(nrow(file.listing) > 1) { 
-    stop("More than one file is being returned by your title. Please use the exact text. If you are using the exact text, check to make sure that there are not documents with the same name")
-  }
-  if(!is.null(download.type)) { 
-    file.listing$Link <- gsub("txt",download.type,file.listing$Link)
-  }
+    file.listing <- drive.list(title,show.titles=FALSE)
 
-  response <- GET(file.listing$Link,config(token = getOption("drive.auth")))
-  
-  
-  if(download.type == "html") { 
-    file.data <- content(response, as="text")
-  } else { 
-    file.data <- content(response, as="parsed")
-  }
-  
-  return(file.data)
+    if(nrow(file.listing) > 1) {
+        stop("More than one file is being returned by your title. Please use the exact text. If you are using the exact text, check to make sure that there are not documents with the same name")
+    }
+    if(!is.null(download.type)) {
+        file.listing$Link <- gsub("txt",download.type,file.listing$Link)
+    }
+
+    if(download.type == "html") {
+        file.data <- content(response, as="text")
+    } else if(download.type == "xlsx") {
+        file.data <- content(response)
+    } else {
+        warning("The content return is binary xlsx, write it in a file with writeBin() function")
+        file.data <- content(response, as="parsed")
+    }
+
+    return(file.data)
 }
